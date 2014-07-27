72e354288eb4643b53b92472d93e6d1500659ff7
goliatone <burgosemi@gmail.com>
2014-07-26 22:04:38 -0400

    Examples: Remove cruft from makeUsers

--- examples/makeUsers.js
+++ examples/makeUsers.js
@@ -1,9 +1,7 @@
 var fakeit = require('../index');
 var fs = require('fs');
 
-var template = '[{{#repeat 2}}{ "name": "{{findName}}", "handle":"{{userName}}", "address": "{{streetAddress}}", "phone":"{{phoneNumber}}", "company":"{{companyName}}", "img":"{{avatar}}", "bio": "{{sentences 3}}" }{{/repeat}}]';
-template = fs.readFileSync(__dirname+'/user.jtpl', "utf8");
+var template = fs.readFileSync(__dirname+'/user.jtpl', "utf8");
 var result = fakeit.parse(template);
-// var out = JSON.parse(result);
 
 console.log(result);

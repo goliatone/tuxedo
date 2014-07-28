74fa299dbfa0bc6d3a0af7e195401f2aa32522b5
goliatone <burgosemi@gmail.com>
2014-07-27 22:51:57 -0400

    Examples: Fix typo

--- examples/visitors.js
+++ examples/visitors.js
@@ -1,15 +1,15 @@
 var fakeit = require('../index');
 
 
-fakeit.on('complete', function(result){
-	console.log(result);
+fakeit.on('complete', function(result) {
+    console.log(result);
 });
 
-fakeit.fromFile(__dirname+'/visitors.jtpl', {
-	data:{
-		languages:['english', 'spanish', 'french'],
-		parties:['democtrat', 'republican','independent'],
-		createdAt: new Date("Sun Jun 30 2011 03:53:23 GMT+0300 (EEST)"),
-		unixtimestamp:1406416351800
-	}
+fakeit.fromFile(__dirname + '/visitors.jtpl', {
+    data: {
+        languages: ['English', 'Spanish', 'French'],
+        parties: ['democrat', 'republican', 'independent'],
+        createdAt: 'Sun Jun 30 2011 03:53:23 GMT+0300 (EEST)',
+        unixtimestamp: 1406416351800
+    }
 });

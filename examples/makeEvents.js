2a87689e15a07bcb2789b08a56e01bfd54546a2c
goliatone <burgosemi@gmail.com>
2015-09-28 16:37:09 -0400

    update examples

--- examples/makeEvents.js
+++ examples/makeEvents.js
@@ -1,8 +1,13 @@
 var fakeit = require('../index');
-
+var Solver = require('gsolver');
+var extend = require('gextend');
 
 fakeit.on('complete', function(result){
-	console.log(result);
+	var output = JSON.parse(result);
+	var context = extend({}, output);
+	var solver = new Solver();
+	output = solver.solve(output, context);
+	console.log(JSON.stringify(output, null, 4));
 });
 
 fakeit.fromFile(__dirname + '/event.jtpl');
